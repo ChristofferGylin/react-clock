d17f9e5a7ced31ed0a9e2120453b8de17acf4a34
Christoffer <christoffer@christoffergylin.com>
2023-01-05 17:00:45 +0100

    Added timezone and freeze functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
-import { useEffect, useState } from "react";
+import Clock from "./Clock";
 import "./App.css";
 
-
 function App() {
-
+  console.log('Render app')
   return (
-    <Clock city="Stockholm" timeZone="Stockholm/Grrenwitch" />
+    <Clock city='Local Time' timeZone={undefined} />
   )
-
-}
-
-const Clock = (props) => {
-
-  const [time, setTime] = useState('00:00:00');
-
-  const updateClock = () => {
-
-    const newTime = new Date().toLocaleTimeString();
-
-    if (newTime !== time) {
-
-      setTime(newTime);
-
-    }
-
-    requestAnimationFrame(updateClock);
-
-  }
-
-  useEffect(() => {
-
-    //setTime(date.toLocaleTimeString('en-GB', { timeZone: 'Europe/London' }));
-    requestAnimationFrame(updateClock);
-
-  }, [])
-
-  return <div className="clock"><div className="location">{props.city}</div><div className="time"><span className="timeDigit">{time[0]}</span><span className="timeDigit">{time[1]}</span><span className="timeDivider">{time[2]}</span><span className="timeDigit">{time[3]}</span><span className="timeDigit">{time[4]}</span><span className="timeDivider">{time[5]}</span><span className="timeDigit">{time[6]}</span><span className="timeDigit">{time[7]}</span></div></div>;
 
 }
 
